3c6226e04f3cd0441a0281f87ccb9b20b07839b5
justgo86 <justgo86@121.130.142.180>
2016-10-18 01:11:20 +0900

    midnight study

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -37,28 +42,57 @@ public class RequestHandler extends Thread {
         	}
         	
         	String [] tokens = line.split(" ");
-        	for(int i = 0 ; i < tokens.length ; i++){
-        		log.debug("tokens["+i+"] - : {}",tokens[i]);
-        	}
+        	int contentLength = 0;
         	
         	while(!line.equals("")){
         		log.debug("header : {}", line);
         		line = bfReader.readLine();
+        		if(line.contains("Content-Length")){
+        			contentLength = getContentLength(line);
+        		}
         	}
-        	
-            DataOutputStream dos = new DataOutputStream(out);
-            
-            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
-            
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-            
+        	String url = tokens[1];
+        	if("/user/create".equals(url)){
+        		String body = IOUtils.readData(bfReader, contentLength);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+        		
+        		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+        		log.debug("user : {}", user);
+        		DataOutputStream dos = new DataOutputStream(out);
+        		response302Header(dos, "/index.html");
+        	}else{
+		        DataOutputStream dos = new DataOutputStream(out);
+		        
+		        byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+		        
+		        response200Header(dos, body.length);
+		        responseBody(dos, body);
+        	}
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+	private void response302Header(DataOutputStream dos, String url) {
+		// TODO Auto-generated method stub
+		try{
+			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+			dos.writeBytes("Location : "+ url + "\r\n");
+			dos.writeBytes("\r\n");
+		}catch(IOException e){
+			log.error(e.getMessage());
+		}
+		
+	}
+
+	private int getContentLength(String line) {
+		// TODO Auto-generated method stub
+    	
+    	String[] headerTonkens = line.split(":");
+    	return Integer.parseInt(headerTonkens[1].trim());
+	}
+
+	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
