7931cbbbe1c0c9c70fc892bb16415f8ae47b074f
justgo86 <justgo86@naver.com>
2016-10-17 18:11:37 +0900

    진행중

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,59 +2,85 @@ package webserver;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
+
 public class RequestHandler extends Thread {
-    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	
+	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	
+	private Socket connection;
+	
+	public RequestHandler(Socket connectionSocket) {
+		this.connection = connectionSocket;
+	}
+	
+	public void run() {
+		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
+		
+		try(InputStream in = connection.getInputStream(); OutputStream out= connection.getOutputStream()){
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			
+			String line = br.readLine();
+			log.debug("request line : {}", line);
+			
+			if(line == null){
+				return;
+			}
+			
+			String[] tokens = line.split(" ");
+			int contentLength = 0;
+			
+			while(!line.equals("")){
+				line = br.readLine();
+				if(line.contains("Content-Length")){
+					contentLength = getContentLength(line);
+				}
+				log.debug("header : {}", line);
+			}
+			
+			String url = tokens[1];
+			
+			//url 판별
+			if("/user/create".equals(url)){
+				String body = IOUtils.readData(br, contentLength);
+				
+				Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+				User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+				log.debug("User : {}", user);
+				url = "/index.html";
+			}else{
+				DataOutputStream dos = new DataOutputStream(out);
+				byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+				response200Header(dos, body.length);
+				responseBody(dos, body);
+			}
+		}catch (IOException e) {
+			// TODO: handle exception
+			log.equals(e.getMessage());
+		}
+	}
+	
+	private int getContentLength(String line) {
+		// TODO Auto-generated method stub
+		String[] headerTokens = line.split(":");
+		return Integer.parseInt(headerTokens[1].trim());
+	}
 
-    private Socket connection;
-
-    public RequestHandler(Socket connectionSocket) {
-        this.connection = connectionSocket;
-    }
-
-    public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
-
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-        	
-        	BufferedReader bfReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-        	String line = bfReader.readLine();
-        	
-        	if(line == null){
-        		return ;
-        	}
-        	
-        	String [] tokens = line.split(" ");
-        	for(int i = 0 ; i < tokens.length ; i++){
-        		log.debug("tokens["+i+"] - : {}",tokens[i]);
-        	}
-        	
-        	while(!line.equals("")){
-        		log.debug("header : {}", line);
-        		line = bfReader.readLine();
-        	}
-        	//asdfasdf
-        	
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World!!!!! WTH\n".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
